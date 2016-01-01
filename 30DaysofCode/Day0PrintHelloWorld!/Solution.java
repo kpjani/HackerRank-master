c1bdbde29dd4e6489c9c41c68f50b36452dc3bd9
KetanJani <kpjani@ncsu.edu>
2016-01-01 18:05:24 -0500

    .

---
+++ 30DaysofCode/Day0PrintHelloWorld!/Solution.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class HelloWorld {
+
+       public static void main(String[] args) {
+            System.out.println("Hello World.");
+            System.out.println("Welcome to 30 Days of Code.");
+       }
+}
+
+
