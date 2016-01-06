8bdef694005b04b6dd81eb36cb63051aa7a68b5d
KetanJani <kpjani@ncsu.edu>
2016-01-06 18:10:18 -0500

    .

---
+++ 30DaysofCode/Day6Let'sReview!/Solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        if((n>=1) && (n<=100)){
+            for(int i=1;i<=n;i++){
+                for(int j=n;j>i;j--){
+                    System.out.print(" ");
+                }
+                for(int j=i;j>0;j--){
+                    System.out.print("#");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
